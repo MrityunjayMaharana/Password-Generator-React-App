850d24ca4de7dfdd172caa066c012aa73696ab5d
Mrityunjay <mrityunjaymaharana8@gmail.com>
2024-06-28 13:21:16 +0530

    Password Generator Commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/Password-Generator--React-App/'
+  base:'/Password-Generator-React-App/'
 })
